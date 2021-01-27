82c2c16106b2af79573e36a8a25a1e776f73df92
student <student@ip-172-31-58-65.us-west-2.compute.internal>
2021-01-26 19:54:07 -0800

    Im working on print and popups!

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,10 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+String code = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, tell them to sign up for classes at The League.
+if (code.equalsIgnoreCase("yes")){
+	JOptionPane.showMessageDialog(null, "Good for you!" );
+}
+else {
+	JOptionPane.showMessageDialog(null, "You should sign up for classes at the League!" );
+}
+// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
 }
