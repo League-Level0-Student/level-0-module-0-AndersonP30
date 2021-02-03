7f616c6052121a1233f1474a2b1f853b03aa7a63
student <student@ip-172-31-63-204.us-west-2.compute.internal>
2021-02-02 20:03:26 -0800

    I finished the maze.

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,16 +1,31 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+ String code = JOptionPane.showInputDialog("What is the color of the sky?");
+ if (code.equalsIgnoreCase("blue")){
+		JOptionPane.showMessageDialog(null, "Correct!");
+		score += 1;
+ }
+ else 
+	 score -= 1;
 				// 2.  Ask the user a question 
-				
+String q2 = JOptionPane.showInputDialog("How many letters are in the alphabet?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+// || means or
+if (q2.equalsIgnoreCase("twenty six")|| q2.equalsIgnoreCase("26")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score += 1;
+}
+else 
+	 score -= 1;
+JOptionPane.showMessageDialog(null, "Your final score was " + score + "!");
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
