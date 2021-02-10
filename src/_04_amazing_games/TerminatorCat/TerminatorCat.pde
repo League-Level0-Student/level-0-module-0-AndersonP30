c5438fbec45a18f055043e4a0a61633a5111bedf
student <student@ip-172-31-62-162.us-west-2.compute.internal>
2021-02-09 20:05:02 -0800

    I finished the cat and dragon.

--- src/_04_amazing_games/TerminatorCat/TerminatorCat.pde
+++ src/_04_amazing_games/TerminatorCat/TerminatorCat.pde
@@ -9,8 +9,8 @@
 // This line of code creates a variable to hold your faccate picture
 // You will use it later.
 PImage cat;
-int x=0;
-int y=0;
+int x=291;
+int y=276;
 
 void setup() {
   
@@ -19,12 +19,13 @@ void setup() {
 cat = loadImage("cat.jpg");
   
 // 3. Set the size of the sketch. Make it big enough to show the cat you chose.
-
+size(600,600);
+cat.resize(600,600);
 // 4. Resize the cat so it is the same size as the sketch
 
 // 5. DRAW CAT.    Use the background() command to make the cat the background of the sketch
 //    Run the program to see if the cat is drawn. Get this working before you go on.
- 
+ background(cat);
 
   }
 
@@ -43,25 +44,37 @@ void draw() {
 
 // 8. DRAW CIRCLES.
 //     The circles will have black lines around them by default. Put noStroke(); here to remove them.
-
+noStroke();
 // 9.  COLOR.  Set the color of your ellipse to the laser color you would like
 //    Remember to use the   fill()  command to set colors.
+fill(200, 0, 0);
+ellipse(x+70, y+14, 25, 25);
 
+fill(200, 0, 0);
+ellipse(x, y, 25, 25);
+
+
+if (x >= width){
+x = (291);
+y = (276);
+background(cat);
+}
+}
 
 // 10 Use the ellipse() command to draw a circle on the eye (you will have to guess its size). 
 //    Use the x and y variables you just created to place the ellipse in the correct location.
 //                  The ellipse command looks like this:
-                ellipse(x, y, width, height);
 // Run the program to make sure it is in the right place and is the right size.
-}
+
 
 // 11.  LASER BEAM.  This code will make your ellipse move down and to the right when you press 
 //      the space bar. Run the program to test it.
 //      If you want it to move to the left, change to x-1=.
 void keyPressed() {
-    x+=1;
-    y+=1;
-    
+    x+=10;
+    y+=10;
+
+   
 // 12.  If you want them to go faster, add more than one each time the key is pressed    
 }
  
--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -14,37 +14,44 @@ public class DragonFight {
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 
 		// 1. Create some variables to hold health levels
-		
+int playerHealth = 100;
 			// playerHealth to store your health - set it equal to 100
-	
+int dragonHealth = 100;
 			// dragonHealth to store the dragon's health - set it equal to 100
 
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
 		
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
-	
+int playerAttack = 0;
 			// dragonAttack to store the damage the dragon's attack will do - set it equal
 			// to 0 for now.
-
+int dragonAttack = 0;
 		
 		//  This while statement will cause the game attack code to repeat
 		while (true) {
 
 			// THE PLAYER ATTACKS THE DRAGON
-
+			String attack1 = JOptionPane.showInputDialog("Do you want to do a yell attack or a kick attack?");
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
 	
 				// 4. If they typed in "yell":
-	
+	if (attack1.equalsIgnoreCase("yell")) {
+		playerAttack = ran.nextInt(30);
+}
+	if (attack1.equalsIgnoreCase("kick")) {
+		playerAttack = ran.nextInt(40);
+	}
 					  // -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					  // ran.nextInt(10)
-			
+			dragonHealth -= playerAttack;
 				// 5. If they typed in "kick":
 	
 					  // -- Find a random number between 0 and 25 and store it in playerAttack.
-		
+	dragonAttack = ran.nextInt(35);
+	
+	playerHealth -= dragonAttack;
 				// 6. Subtract the player attack value from the dragon's health
 
 			// THE DRAGON RETALIATES
@@ -58,14 +65,20 @@ public class DragonFight {
 				// 9. If the player's health is less than or equal to 0, the game is over,
 				//    call the playerLost() method
 	
+			if (playerHealth <= 0) {
+				playerLost();
+			}
+			if (dragonHealth <= 0) {
+				dragonLost();
 			
+			}
 				// 10. If the dragon's health is less than or equal to 0, the game is over,
 				//     call the dragonLost() method
 
-			
+			JOptionPane.showMessageDialog(null,"You have " + playerHealth + " health. The dragon has " +dragonHealth + " health");
 				// 11.  Pop up a message that tells us how much health the player and
 				// 		dragon have left.
-
+			JOptionPane.showMessageDialog(null, "You lost " + dragonAttack + " health. You dealt " + playerAttack + " damage.");
 			
 			// (Bonus: Also display the amount of health that was lost by each in this
 			// round)
@@ -77,12 +90,13 @@ public class DragonFight {
 
 	static void playerLost() {
 		// 11. Tell the player that they have been defeated by the dragon and have no treasure
-
+		JOptionPane.showMessageDialog(null,"You died :(");
 
 		System.exit(0);   //This code ends the program
 	}
 
 	static void dragonLost() {
+		JOptionPane.showMessageDialog(null,"You killed the dragon!");
 		// 12. Tell the user that the dragon has been defeated and they get a ton of gold!
 
 		System.exit(0);   //This code ends the program
